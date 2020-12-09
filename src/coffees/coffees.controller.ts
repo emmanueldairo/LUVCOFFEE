897fdb9f66a38c67c7ca846a47f85bef4e24851c
emmanueldairo <emmanueldairo@cmail.carleton.ca>
2020-12-08 23:48:22 -0500

    patch2

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -13,7 +13,7 @@ export class CoffeesController {
     }
     @Get(`:id`)
     findOne(@Param('id') id: number){
-        console.log(typeof id)
+         console.log(typeof id)
         return this.coffeeService.findOne(''+id);
     }    
     @Post()
